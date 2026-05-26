43051dc04d829a7c26318c62aad1a83441e43db2
Tuan Anh <tuananh7088@gmail.com>
2026-05-26 13:44:19 +0700

    video 36

--- src/components/todo/TodoInput.jsx
+++ src/components/todo/TodoInput.jsx
@@ -2,11 +2,23 @@
 const TodoInPut = (props) => {
     console.log(">>> check props", props)
     const { AddNewTodo } = props;
-    AddNewTodo("Tuan Anh");
+    // AddNewTodo("Tuan Anh");
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name)
+    }
+
     return (
         <div className='todo-input'>
-            <input type="text" name="" id="" placeholder='Enter your task' />
-            <button>Add</button>
+            <input type="text" name="" id="" placeholder='Enter your task'
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
